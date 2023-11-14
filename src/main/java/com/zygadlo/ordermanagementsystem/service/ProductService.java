2b7ff4ac5179a9f87926e0af512cad72079decd1
DawidoXxx <zygadloda@gmail.com>
2023-11-14 21:45:05 +0100

    code refactored in Product Service and changes in HomePage and NoShopPage

--- src/main/java/com/zygadlo/ordermanagementsystem/service/ProductService.java
+++ src/main/java/com/zygadlo/ordermanagementsystem/service/ProductService.java
@@ -2,117 +2,112 @@ package com.zygadlo.ordermanagementsystem.service;
 
 import com.zygadlo.ordermanagementsystem.exception.NoFileStructureFoundException;
 import com.zygadlo.ordermanagementsystem.exception.StorageException;
-import com.zygadlo.ordermanagementsystem.model.DataFromDatabase;
-import com.zygadlo.ordermanagementsystem.model.FileSettings;
-import com.zygadlo.ordermanagementsystem.model.Product;
-import com.zygadlo.ordermanagementsystem.model.ProductFromSeller;
+import com.zygadlo.ordermanagementsystem.model.*;
 import com.zygadlo.ordermanagementsystem.repository.*;
+import org.apache.logging.slf4j.SLF4JLogger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 @Service
 public class ProductService {
 
+    private static final String EXCEL_EXTENSION = ".xlsx";
+    private static final String CSV_EXTENSION = ".csv";
+    private static final Logger logger = LoggerFactory.getLogger(ProductService.class);
+
     @Value("${upload.database}")
-    private String databaseFilesPath;
-    private FileStructureRepository fileStructureRepository;
-    private CsvRepository csvRepository;
-    private ExcelRepository excelRepository;
-    private ProductsRepository productRepository;
-    private SellerRepository sellerRepository;
+    public String databaseFilesPath;
+    private final FileStructureRepository fileStructureRepository;
+    private final CsvRepository csvRepository;
+    private final ExcelRepository excelRepository;
+    private final ProductsRepository productRepository;
+    private final SellerRepository sellerRepository;
+    private final DataBaseUpdateInfo dataBaseUpdateInfo;
 
     public ProductService(FileStructureRepository fileStructureRepository, CsvRepository csvRepository,
                           ExcelRepository excelRepository, ProductsRepository productRepository,
-                          SellerRepository sellerRepository) {
+                          SellerRepository sellerRepository, DataBaseUpdateInfo dataBaseUpdateInfo) {
         this.fileStructureRepository = fileStructureRepository;
         this.csvRepository = csvRepository;
         this.excelRepository = excelRepository;
         this.productRepository = productRepository;
         this.sellerRepository = sellerRepository;
+        this.dataBaseUpdateInfo = dataBaseUpdateInfo;
     }
 
-    private void readProductsFromDatabaseFiles() throws StorageException,NoFileStructureFoundException{
+    public Map<String,DataFromDatabase> readProductsFromDatabaseFiles(File database,FileSettings fileSettings) throws StorageException,NoFileStructureFoundException{
+        if (fileSettings==null)
+            throw new NoFileStructureFoundException("There is no "+database.getName().split("\\.")[0]+" DataBase file structure");
 
-        //get database settings
-        List<FileSettings> listOfDatabaseSettings = fileStructureRepository.findAll();
+        String extension = database.getName().substring(database.getName().lastIndexOf("."));
 
-        Map<String, DataFromDatabase> productMap;
-
-        //get databases from folder
-        File[] databases = getDataBaseFiles();
-        if (databases==null)
-            throw new StorageException("There is no database files");
-
-
-        for (File database:databases) {
-            FileSettings fileSettings = fileStructureRepository.findByFileName(database.getName().split("//.")[0]+"DB");
-
-            //read if its Excel file and there is specific to this name file settings else we don't know how to read file
-            if (database.getName().contains(".xlms")){
-                if (fileSettings==null)
-                    throw new NoFileStructureFoundException("There is no "+database.getName().split("//.")[0]+" DataBase file structure");
-                productMap = excelRepository.getDataFromDatabase(database,fileSettings);
-            }
-
-            //same as above just it about csv file
-            if (database.getName().contains(".csv")&&fileSettings!=null){
-                productMap = csvRepository.getDataFromDatabase(database,fileSettings);
-                //saveToMongoDatabase(productMap);
-            }
-            else
-                throw new NoFileStructureFoundException("There is no "+database.getName().split("//.")[0]+" DataBase file structure");
-
-            if (!productMap.isEmpty())
-                saveToMongoDatabase(productMap,database.getName().substring(0,database.getName().indexOf(".")));
-
-            productMap.clear();
-        }
+        //getting right repository for given database file
+        return switch (extension){
+            case EXCEL_EXTENSION -> excelRepository.getDataFromDatabase(database,fileSettings);
+            case CSV_EXTENSION -> csvRepository.getDataFromDatabase(database,fileSettings);
+            default -> throw new StorageException("Unsupported database file format: "+extension);
+        };
     }
 
-    private File[] getDataBaseFiles(){
+    public File[] getDataBaseFiles(){
         File directory = new File(databaseFilesPath);
-        return directory.listFiles();
+        return Optional.ofNullable(directory.listFiles()).orElse(new File[0]);
     }
 
     private void saveToMongoDatabase(Map<String,DataFromDatabase> mapToSaveToDB,String name){
-        int priority;
 
-        if(sellerRepository.findByName(name)!=null)
-            priority = sellerRepository.findByName(name).getPriority();
-        else
-            priority = 1;
+        //get default value of 1 if not saved,
+        //priority is a second value to compare after price are the same
+        int priority = sellerRepository.findByName(name) != null ?
+                sellerRepository.findByName(name).getPriority() : 1;
 
-        mapToSaveToDB.forEach((x,y)->{
-            if (productRepository.existsByEan(x)){
-                Product product = productRepository.findByEan(x);
-                ProductFromSeller productFromSeller = new ProductFromSeller(name,priority,y.getPrice(),y.getSallersCode());
-                product.addProductFromSeller(productFromSeller);
-                product.addNameToSearchField(y.getNameOfProduct());
-                productRepository.save(product);
-            }else {
-                Product product = new Product(x, y.getNameOfProduct());
-                product.addProductFromSeller(new ProductFromSeller(name,priority,y.getPrice(),y.getSallersCode()));
-                productRepository.save(product);
-            }
+        mapToSaveToDB.forEach((ean,data)->{
+            Product product = productRepository.existsByEan(ean)?
+                    productRepository.findByEan(ean):
+                    new Product(ean,data.getNameOfProduct());
+
+            ProductFromSeller productFromSeller = new ProductFromSeller(name,priority,data.getPrice(),data.getSallersCode());
+            product.addProductFromSeller(productFromSeller);
+            product.addNameToSearchField(data.getNameOfProduct());
+            productRepository.save(product);
         });
     }
 
-    public void updateProducts(){
+    public void updateProducts(LocalDateTime now){
+        productRepository.deleteAll();
+        DateTimeFormatter dtf=DateTimeFormatter.ofPattern("yyyy/MM/dd - HH:mm");
 
-    }
+        try {
+            File[] databases = getDataBaseFiles();
+            if (databases==null||databases.length==0) {
+                logger.warn("No database files found for update");
+                return;
+            }
 
-    private List<FileSettings> getNeededFileSettings(File[] databases){
-        List<FileSettings> returnList = new ArrayList<>();
-        for (File file:databases) {
-            returnList.add(fileStructureRepository.findByFileName(file.getName().split("\\.")[0]));
+            for (File database:databases){
+                String nameOfDatabase = database.getName().split("\\.")[0];
+                FileSettings fileSettings = fileStructureRepository.findByFileName(nameOfDatabase+"DB");
+                Map<String, DataFromDatabase> productMap = readProductsFromDatabaseFiles(database,fileSettings);
+                saveToMongoDatabase(productMap,nameOfDatabase);
+                dataBaseUpdateInfo.save(new UpdateSellersDate(nameOfDatabase,(dtf.format(now))));
+            }
+        }catch (StorageException | NoFileStructureFoundException stEx){
+            logger.error("An error occurred during product update", stEx);
         }
-        return returnList;
     }
-
 }
+
+//    private List<FileSettings> getNeededFileSettings(File[] databases){
+//        List<FileSettings> returnList = new ArrayList<>();
+//        for (File file:databases) {
+//            returnList.add(fileStructureRepository.findByFileName(file.getName().split("\\.")[0]));
+//        }
+//        return returnList;
+//    }
